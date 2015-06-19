4edf336210c2788073b25dc0859ab380dd72bbda
Miroslav Bajtoš <miroslav@strongloop.com>
2015-06-19 10:24:18 +0200

    Prevent double start/stop call
    Modify start & stop methods to queue subsequent calls and ensure that
    the C++ bindings are invoked only once.
    
    Keep track whether the debugger is enabled/disabled and bypass the C++
    API when starting an already started session.

--- index.js
+++ index.js
@@ -7,21 +7,79 @@ var debuglogEnabled = require('debug')('strong-debugger').enabled;
 var workerPath = require.resolve('./backend/context.js');
 var scriptRoot = path.dirname(workerPath) + path.sep;
 
+var startCallbacks = [];
+var stopCallbacks = [];
+var listeningOnPort = -1;
+
 /**
  * Start the background thread providing TCP server for DevTools protocol.
  * @param {Number} port
- * @param {Function<Error=,Number>} callback
+ * @param {Function<Error=,Number>} callback The first argument is the error
+ * (if any), the second argument is the actual port number where the debugger
+ * is listening on.
  */
 exports.start = function(port, cb) {
-  dbg.start({
-    port: port,
-    scriptRoot: scriptRoot,
-    debuglogEnabled: debuglogEnabled
-  }, cb);
+  if (!cb) throw new Error('You must supply a callback argument.');
+
+  if (listeningOnPort > 0) {
+    // The background thread is already running.
+    return process.nextTick(function() {
+      cb(null, listeningOnPort);
+    });
+  }
+
+  startCallbacks.push(cb);
+  if (startCallbacks.length > 1) return;
+
+  try {
+    dbg.start({
+      port: port,
+      scriptRoot: scriptRoot,
+      debuglogEnabled: debuglogEnabled
+    }, onStarted);
+  } catch (err) {
+    startCallbacks = [];
+    throw err;
+  }
+
+  function onStarted(err, port) {
+    if (!err && port !== undefined) listeningOnPort = port;
+
+    var list = startCallbacks;
+    startCallbacks = [];
+    list.forEach(function(cb) {
+      cb(err, port);
+    });
+  }
 };
 
 /**
  * Stop the background thread.
  * @param {Function<Error=>} callback
  */
-exports.stop = dbg.stop;
+exports.stop = function(cb) {
+  if (listeningOnPort < 0) {
+    // The background thread is not running.
+    return process.nextTick(cb);
+  }
+
+  stopCallbacks.push(cb);
+  if (stopCallbacks.length > 1) return;
+
+  try {
+    dbg.stop(onStopped);
+  } catch (err) {
+    stopCallbacks = [];
+    throw err;
+  }
+
+  function onStopped(err) {
+    listeningOnPort = -1;
+
+    var list = stopCallbacks;
+    stopCallbacks = [];
+    list.forEach(function(cb) {
+      cb(err);
+    });
+  }
+};
